28015593455daad570d6f6ac2a3a11514b57122a
Jason Data <jason.data@roadbytes.me>
2015-08-19 13:24:02 -0700

    added registeration feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+
+    if @user.save
+      session[:id] = @user.id
+      flash[:notice] = "Welcome"
+      redirect_to user_cards_path(@user)
+    else
+      flash[:error] = "Error registering"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
